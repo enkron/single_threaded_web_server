1f88a17b6385f0a646df99695a46a69fab8a74f3
Sergei Belokon <srj.belokon@gmail.com>
2021-07-24 15:03:29 +0300

    Added port binding part
    This part creates TcpListener type instances, which binds to
    127.0.0.1:7878 addr and listen for incoming TCP stream

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::net::TcpListener;
+
+// Listen at the addr 127.0.0.1:7878 for incoming TCP streams.
 fn main() {
-    println!("Hello, world!");
+    // the `bind` func in TcpListener acts like `new` func:
+    // it returns a new TcpListener instance.
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for _stream in listener.incoming() {
+        let _stream = _stream.unwrap();
+
+        println!("Connection established.");
+    }
 }
