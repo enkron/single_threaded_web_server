70f43b1cb1879246c3585f44528b50cb3232095a
Sergei Belokon <srj.belokon@gmail.com>
2021-07-27 23:55:34 +0300

    Removed redudant writes into the stream

--- src/main.rs
+++ src/main.rs
@@ -28,35 +28,23 @@ fn handle_connection(mut stream: TcpStream) {
 
     let get = b"GET / HTTP/1.1\r\n";
 
-    if buffer.starts_with(get) {
-        let contents = fs::read_to_string("hello.html").unwrap();
-
-        // holds success message's data
-        let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Lenght: {}\r\n\r\n{}",
-            contents.len(),
-            contents
-        );
-
-        // convert the bytes in the buffer to a string and print it
-        //println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-
-        // Response
-        stream.write(response.as_bytes()).unwrap(); // sends bytes directly
-                                                    // down the connection
-        stream.flush().unwrap(); // prevent the program from continuing until
-                                 // all the bytes are written to the connection.
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
-        let response = format!(
-            "{}\r\nContent-Length: {}\r\n\r\n{}",
-            status_line,
-            contents.len(),
-            contents
-        );
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap(); // sends bytes directly
+                                                // down the connection
+    stream.flush().unwrap(); // prevent the program from continuing until
+                             // all the bytes are written to the connection.
 }
